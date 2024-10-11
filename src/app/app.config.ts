57f924c0e00d5c90050cd3b56247c3bc4e2a9190
Carlos Serrano <csersan210@iesfidiana.es>
2024-10-11 14:02:14 +0200

    firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment.development';
+import { AngularFireModule } from '@angular/fire/compat';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +12,9 @@ export const appConfig: ApplicationConfig = {
      provideRouter(routes, 
      withComponentInputBinding(),
      withRouterConfig({paramsInheritanceStrategy: 'always'})),
-     provideHttpClient(), provideFirebaseApp(() => initializeApp({"projectId":"recipes-ee917","appId":"1:808578601351:web:a808c0365a8e563c6d0322","storageBucket":"recipes-ee917.appspot.com","apiKey":"AIzaSyD_bJ3S_E2i8SrxOax9KkqJ0ZjQTjmwC3Y","authDomain":"recipes-ee917.firebaseapp.com","messagingSenderId":"808578601351","measurementId":"G-0PDSYNZEQW"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
-     
+     provideHttpClient(),
+     importProvidersFrom(AngularFireModule.initializeApp(environment.firebaseConfig))]
+
 };
+
+//ng add @angular/fire
